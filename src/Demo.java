1225727b71cc150723380c0f9f14de1fdb99567b
Damian <damian.eggert@wp.pl>
2019-10-29 20:33:53 +0100

    +DriverObject

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
 
     public static void main (String[] args) {
-    //selenium code
+        //selenium code
+
+        // creating driver object
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\WebDrivers");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
     }
 }
